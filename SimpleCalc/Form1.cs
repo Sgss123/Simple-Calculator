a6fff0fd95c79d4d9e2f47311c76b5d9115c4d84
Sgss123 <103869422+Sgss123@users.noreply.github.com>
2025-04-10 11:29:39 +0800

    feat: 添加SimpleCalc计算器应用的基本框架
    此次提交包含了SimpleCalc计算器应用的基本框架，包括主窗体、配置文件、资源文件以及项目文件。应用支持基本的加、减、乘、除运算，并提供了用户界面用于输入操作数和显示结果。

---
+++ SimpleCalc/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCalc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double num1,
+            num2,
+            result;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Text = "Simple Calculator";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            num1 = Convert.ToDouble(textBox1.Text);
+            num2 = Convert.ToDouble(textBox2.Text);
+            result = num1 + num2;
+            label3.Text = result.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            num1 = Convert.ToDouble(textBox1.Text);
+            num2 = Convert.ToDouble(textBox2.Text);
+            result = num1 - num2;
+            label3.Text = result.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            num1 = Convert.ToDouble(textBox1.Text);
+            num2 = Convert.ToDouble(textBox2.Text);
+            result = num1 * num2;
+            label3.Text = result.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            num1 = Convert.ToDouble(textBox1.Text);
+            num2 = Convert.ToDouble(textBox2.Text);
+            try
+            {
+                result = num1 / num2;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Error: " + ex.Message,
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+            }
+            label3.Text = result.ToString();
+        }
+    }
+}
